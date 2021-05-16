015666737c737990d04537d766d36152dba40b52
Pisun-afk <84274513+Pisun-afk@users.noreply.github.com>
2021-05-16 16:06:34 +0300

    Add files via upload
    Это мой 6 проэкт очень сложно но прикольно надеюсь вам понравиться версия 1.1.1

---
+++ youtube_contest.py
@@ -0,0 +1,16 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QLabel, QMessageBox, QRadioButton
+app = QApplication([])
+main_win = QWidget()
+main_win.setWindowTitle("Конкурс от Crazy People")
+question = QLabel("В каком году канал <<золтую кнопку>> от YouTube?")
+btn_answer1 = QRadioButton("2005")
+btn_answer2 = QRadioButton("2010")
+btn_answer3 = QRadioButton("2015")
+btn_answer4 = QRadioButton("2020")
+layout_main = QVBoxLayout()
+layout_main.addWidget(question, alignment = Qt.AlignCenter)
+layout_main.addWidget(btn_answer1, alignment = Qt.AlignCenter)
+layout_main.addWidget(btn_answer2, alignment = Qt.AlignCenter)
+layout_main.addWidget(btn_answer3, alignment = Qt.AlignCenter)
+layout_main.addWidget(btn_answer4, alignment = Qt.AlignCenter)
